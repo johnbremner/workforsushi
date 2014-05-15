01654698ca767a887c8d09de24e3fd54fdb8f1db
Jordan <jordandietch@gmail.com>
2014-05-14 21:42:47 -0500

    Added application route to menu page
    Added an application route to the menu page, now just need to figure
    out what it should look like. I’m thinking we keep the photo up at the
    top with the word Menu overlaid. The menu is going to be a list of
    food. An affix will be in the right rail that allows a searcher to
    toggle between the different menus.

--- app/main/views.py
+++ app/main/views.py
@@ -1,24 +1,12 @@
-from flask import render_template, session, redirect, url_for
+from flask import render_template
 from . import main
-from .. import db
-from .forms import NameForm
-from ..models import User
 
 
-@main.route('/', methods=['GET', 'POST'])
+
+@main.route('/')
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            session['known'] = False
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        form.name.data = ''
-        return redirect(url_for('.index'))
-    return render_template('index.html',
-                           form=form, name=session.get('name'),
-                           known=session.get('known', False))
+    return render_template('index.html')
+
+@main.route('/menu')
+def menu():
+    return render_template('menu.html')
