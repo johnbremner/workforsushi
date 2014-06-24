e91e1874054940d2221562d9309250faa4bf7731
Jordan <jordandietch@gmail.com>
2014-06-24 07:46:26 -0700

    Added parallax image scrolling and some photos

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,11 +1,11 @@
 $(function() {
 
-    $(window).scroll(function(){
-        if( $('body').scrollTop() > 35 ){
-            $('.navbar').addClass('navbar-fixed-top');
-        } else {
-            $('.navbar').removeClass('navbar-fixed-top');
-        }
-    });
+    // $(window).scroll(function(){
+    //     if( $('body').scrollTop() > 35 ){
+    //         $('.navbar').addClass('navbar-fixed-top');
+    //     } else {
+    //         $('.navbar').removeClass('navbar-fixed-top');
+    //     }
+    // });
 
 });
