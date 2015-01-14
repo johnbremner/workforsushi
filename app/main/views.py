e96353b5641951c7c0f879a15a493dcdc9210450
Jordan <jordandietch@gmail.com>
2015-01-13 21:22:35 -0600

    Add hours and directions page

--- app/main/views.py
+++ app/main/views.py
@@ -5,10 +5,14 @@ from . import main
 def index():
     return render_template('index.html')
 
-@main.route('/about-robata-grill')
+@main.route('/about')
 def about():
     return render_template('about.html')
 
-@main.route('/robata-grill-menu')
+@main.route('/menu')
 def menu():
     return render_template('menu.html')
+
+@main.route('/hours-and-directions')
+def hours():
+    return render_template('hours-and-directions.html')
