26d4fe87f893e72ef68e4e739b37c9d3856208f7
Jordan <jordandietch@gmail.com>
2015-01-17 16:25:18 -0600

    Add contact page and contact form

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,7 @@
-from flask import render_template
+from flask import render_template, current_app, flash, redirect, url_for
 from . import main
+from forms import ContactForm
+from ..email import send_email
 
 @main.route('/')
 def index():
@@ -16,3 +18,19 @@ def menu():
 @main.route('/hours-and-directions')
 def hours():
     return render_template('hours-and-directions.html')
+
+@main.route('/contact', methods=['GET', 'POST'])
+def contact():
+    contact_form = ContactForm()
+    if contact_form.validate_on_submit():
+        name = contact_form.name.data
+        email = contact_form.email.data
+        phone = contact_form.phone.data
+        message = contact_form.message.data
+        if current_app.config['APP_ADMIN']:
+                send_email(current_app.config['APP_ADMIN'], 'Robata Grill Inquiry',
+                           'mail/message', name=name, email=email, phone=phone, message=message)
+        flash('Your message has been sent')
+        return redirect(url_for('main.contact'))
+    return render_template('contact.html', contact_form = contact_form)
+
--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
+from secrets import Secrets
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -8,10 +10,11 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or Secrets.SECRET_MAIL_USERNAME
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or Secrets.SECRET_MAIL_PASSWORD
+    MAIL_SUBJECT_PREFIX = os.environ.get('MAIL_SUBJECT_PREFIX') or Secrets.SECRET_MAIL_SUBJECT_PREFIX
+    MAIL_SENDER = os.environ.get('MAIL_SENDER') or  Secrets.SECRET_MAIL_SENDER
+    APP_ADMIN = os.environ.get('APP_ADMIN') or  Secrets.SECRET_APP_ADMIN
     BOOTSTRAP_SERVE_LOCAL = True
 
     @staticmethod
